d10cbbce758f62a17fda55a842b067586a99915c
fiorde90 <fiorde_90@hotmail.com>
2024-03-30 19:20:34 -0300

    binarySearch

--- qi_curso_1_algoritmos/src/com/qi/curso_1_algoritmos/c2_binary_search/BinarySearch.java
+++ qi_curso_1_algoritmos/src/com/qi/curso_1_algoritmos/c2_binary_search/BinarySearch.java
@@ -7,31 +7,51 @@ public class BinarySearch {
     private static final Logger logger = Logger.getLogger(BinarySearch.class.getName());
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         int[] array = generateArray();
-        printArray(array);
 
         logger.info("Ingresa el numero a buscar: ");
-        int numberToFind = scanner.nextInt();
-
-        int indexFound = binarySearch(array, numberToFind);
-
-        if (indexFound != -1) {
-            logger.info(String.format("El valor '%s' fue encontrado en el índice %d", numberToFind, indexFound));
-        } else {
-            logger.info(String.format("El valor '%s' no fue encontrado en el array.", numberToFind));
-        }
+        int itemToFind = scanner.nextInt();
+        int indexFound = binarySearch(array, 0, array.length, itemToFind);
+        //int indexFound = binarySearch2(array, itemToFind);
+        informResult(indexFound, itemToFind);
 
         scanner.close();
     }
 
-    private static int binarySearch(int[] array, int element) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == element) {
-                return i;
-            }
+    private static void informResult(int indexFound, int itemToFind) {
+        if (indexFound != -1) {
+            String msg = String.format("El valor '%s' fue encontrado en el índice %d", itemToFind, indexFound);
+            logger.info(msg);
+        } else {
+            String msg = String.format("El valor '%s' no fue encontrado en el array.", itemToFind);
+            logger.info(msg);
         }
+    }
+
+    // personal solution
+    private static int binarySearch(int[] array, int min, int max, int itemToFind) {
+        int middle = min + (max - min) / 2;
+        logger.info(String.valueOf(middle));
+        if (middle == min || middle == max) return -1;
+
+        if (itemToFind < array[middle]) middle = binarySearch(array, min, middle, itemToFind);
+        else if (array[middle] < itemToFind) middle = binarySearch(array, middle, max, itemToFind);
+
+        return middle;
+    }
+
+    // course solution
+    private static int binarySearch2(int[] array, int itemToFind) {
+        int low = 0;
+        int top = array.length;
+        do {
+            int middle = low + (top - low) / 2;
+            int value = array[middle];
+            if (value == itemToFind) return middle;
+            else if (value < itemToFind) low = middle + 1; //incluye valor minimo, excluye valor maximo
+            else top = middle;
+        } while (low < top); //no <=, porque el minimo se incluye, y el maximo se excluye. si fuera <= entraría en un estado roto
         return -1;
     }
 
@@ -44,14 +64,23 @@ public class BinarySearch {
                 string.append(", ");
             }
         }
-        logger.info(string.toString());
+        String msg = string.toString();
+        logger.info(msg);
     }
-    
+
     private static int[] generateArray() {
         int[] array = new int[100];
-
         for (int i = 0; i < 100; i++) {
-            array[i] = i;
+            array[i] = i * 100;
+        }
+        printArray(array);
+        return array;
+    }
+
+    private static int[] generateArrayWithNegativeNumbers() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[array.length - i - 1] = i * -100;
         }
         return array;
     }
