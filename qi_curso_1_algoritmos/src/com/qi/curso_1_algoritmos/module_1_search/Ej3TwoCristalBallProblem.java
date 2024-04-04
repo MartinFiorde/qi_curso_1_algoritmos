e21c271cd524b00b1004c9f78d578f7e32bb4135
fiorde90 <fiorde_90@hotmail.com>
2024-04-04 15:20:51 -0300

    refactor cristalball

--- qi_curso_1_algoritmos/src/com/qi/curso_1_algoritmos/module_1_search/Ej3TwoCristalBallProblem.java
+++ qi_curso_1_algoritmos/src/com/qi/curso_1_algoritmos/module_1_search/Ej3TwoCristalBallProblem.java
@@ -5,61 +5,67 @@ import java.util.logging.Logger;
 
 public class Ej3TwoCristalBallProblem {
     private static final Logger logger = Logger.getLogger(Ej3TwoCristalBallProblem.class.getName());
+    private static final Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         logger.info("ingrese la cantidad de posibles puntos de rotura a analizar");
         boolean[] array = generateArray(scanner.nextInt());
 
-        logger.info("Se buscará el menor valor en el cual las bolas se rompen. Solo se pueden 'ver' booleanos true 2 veces como máximo");
-        int indexFound = twoBallsSearch(array);
-        informResult(indexFound);
+        int indexFoundV1 = twoBallsSearchV1(array);
+        int indexFoundTeacher = twoBallsSearchTeacher(array);
 
-        logger.info("Se buscará el menor valor en el cual las bolas se rompen. Solo se pueden 'ver' booleanos true 2 veces como máximo");
-        int indexFound2 = twoBallsSearch2(array);
-        informResult(indexFound2);
+        if (indexFoundV1==indexFoundTeacher) logger.info("resultados coinciden");
     }
 
     // personal solution
-    private static int twoBallsSearch(boolean[] array) {
-        int length = array.length;
-        int jumpDistance = (int) Math.floor(Math.sqrt(length));
+    private static int twoBallsSearchV1(boolean[] array) {
+        logger.info("Se buscará el menor valor en el cual las bolas se rompen. Solo se pueden 'ver' booleanos true 2 veces como máximo");
+        int jumpDistance = (int) Math.floor(Math.sqrt(array.length));
         int minBreakPoint = -1;
         int i = jumpDistance;
-        for (; i < length; i += jumpDistance) {
+
+        for (; i < array.length; i += jumpDistance) {
             if (array[i]) {
                 minBreakPoint = i;
                 break;
             }
         }
+
         i -= jumpDistance;
 
-        for (int j = i + 1; j < length && j < i + jumpDistance; j++) {
+        for (int j = i + 1; j < array.length && j < i + jumpDistance; j++) {
             if (array[j]) {
                 minBreakPoint = j;
                 break;
             }
         }
 
+        informResult(minBreakPoint);
         return minBreakPoint;
     }
 
     // course solution
-    private static int twoBallsSearch2(boolean[] array) {
+    private static int twoBallsSearchTeacher(boolean[] array) {
+        logger.info("Se buscará el menor valor en el cual las bolas se rompen. Solo se pueden 'ver' booleanos true 2 veces como máximo");
         int jmpAmount = (int) Math.floor(Math.sqrt(array.length));
         int i = jmpAmount;
+
         for (; i < array.length; i += jmpAmount) {
             if (array[i]) {
                 break;
             }
         }
+
         i -= jmpAmount;
 
-        for (int j = i + 1; j < array.length && j <= i + jmpAmount; j++) {
-            if (array[j]) {
-                return j;
+        for (int j = i + 1; j < array.length && j <= i + jmpAmount; j++, i++) {
+            if (array[i]) {
+                informResult(i);
+                return i;
             }
         }
+
+        informResult(-1);
         return -1;
     }
 
