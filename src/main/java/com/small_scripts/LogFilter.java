65110850acf2e30a043d6c96d92a3e2d81e2c918
fiorde90 <fiorde_90@hotmail.com>
2024-08-08 16:48:44 -0300

    added small scripts folder for practices and external implementations

---
+++ src/main/java/com/small_scripts/LogFilter.java
@@ -0,0 +1,30 @@
+package com.small_scripts;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LogFilter {
+    public static void main(String[] args) {
+        String inputFilePath = "C:/Users/Martin F - PC Desk/Desktop/2024-07-25 11-30 us007qi.log"; // Cambia esto con la ruta de tu archivo
+        String outputFilePath = "C:/Users/Martin F - PC Desk/Desktop/2024-07-25 11-30 us007qi - FILTERED AGRO-0002.log"; // Cambia esto con la ruta de tu archivo filtrado
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("AGRO-0002")) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+
+            System.out.println("Log filtrado y guardado en: " + outputFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
