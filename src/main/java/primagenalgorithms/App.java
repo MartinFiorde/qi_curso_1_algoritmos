a7f3d80f0db1c8336a3db19e8b6bf982947cfa36
fiorde90 <fiorde_90@hotmail.com>
2024-09-15 14:59:37 -0300

    try refactor to java 21

--- src/main/java/primagenalgorithms/App.java
+++ src/main/java/primagenalgorithms/App.java
@@ -1,10 +1,7 @@
 package primagenalgorithms;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class App {
     public static void main(String[] args) {
-        log.info("Hello World!");
+        System.out.println("Hello World!");
     }
 }
--- src/test/java/primagenalgorithms/AppTest.java
+++ src/test/java/primagenalgorithms/AppTest.java
@@ -1,22 +1,8 @@
 package primagenalgorithms;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    public void testApp() {
-        assertTrue(true);
-    }
+public class AppTest {
+    // add tests here
 }
